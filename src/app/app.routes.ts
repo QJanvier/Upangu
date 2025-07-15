5876d7af391fcee58bdd586bb58bf67de9d025c6
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-07-15 07:11:13 +0200

    added slug's route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./pages/blog/blog.page').then(m => m.default),
         title: 'Blog',
+    },
+    {
+        path: 'blog/:slug',
+        loadComponent: () => import('./pages/slug/[slug].page').then(m => m.default),
+        title: 'Blog Post',
     }
 ];
