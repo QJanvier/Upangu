52262813882a58d862775e515c5902d603721b9d
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-07-21 13:37:07 +0200

    creating bloglist

--- src/app/blog-list/blog-list.ts
+++ src/app/blog-list/blog-list.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog-list',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './blog-list.html',
   styleUrl: './blog-list.scss'
 })
 export class BlogList {
-
+  posts = [
+    { title: 'First Post', slug: 'first-post' },
+    { title: 'Second Post', slug: 'second-post' }
+  ];
 }
