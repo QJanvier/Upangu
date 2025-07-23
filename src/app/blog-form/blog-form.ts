93223f33214e0f55df42bbbf94ff4179985157c8
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-07-23 16:10:55 +0200

    fixed other error in blog-form

--- src/app/blog-form/blog-form.ts
+++ src/app/blog-form/blog-form.ts
@@ -10,12 +10,13 @@ import { BlogPost } from '../models/post';
 export class BlogForm {
   title: string = '';
   content: string = '';
+  slug: string = "";
 
   @Output() newPost = new EventEmitter<BlogPost>();
 
   submitPost() {
     if (this.title && this.content) {
-      this.newPost.emit({ title: this.title, content: this.content });
+      this.newPost.emit({ title: this.title, slug: this.slug ,content: this.content });
       this.title = '';
       this.content = '';
   }
