844181abdb266e450419225761a028832df06c42
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-07-28 09:34:02 +0200

    removed content, set a new path for about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,5 +13,10 @@ export const routes: Routes = [
         path: ':slug',
         loadComponent: () => import('./pages/slug/[slug].page').then(m => m.BlogPostPage),
         title: 'Blog Post',
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about.page').then(m => m.AboutPage),
+        title: 'About Me',
     }
 ];
