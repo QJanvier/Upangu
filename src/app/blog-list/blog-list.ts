06d0af9d3e2c8b6b2c5c17b0537da6d40b94fb4b
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-07-28 09:17:48 +0200

    added base scss for posts card and form

--- src/app/blog-list/blog-list.ts
+++ src/app/blog-list/blog-list.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { BlogPost } from '../models/post';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog-list',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './blog-list.html',
   styleUrl: './blog-list.scss'
 })
